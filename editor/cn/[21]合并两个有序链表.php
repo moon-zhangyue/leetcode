20d7d77fb7fb2b401b3504bfb556f4961b230b91
zhangyue <575865770@qq.com>
2021-05-30 19:36:10 +0800

    有效的括号

---
+++ editor/cn/[21]合并两个有序链表.php
@@ -0,0 +1,38 @@
+<?php
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+// 
+// Related Topics 链表 
+// 👍 1270 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $l1
+     * @param ListNode $l2
+     * @return ListNode
+     */
+    function mergeTwoLists($l1, $l2) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
