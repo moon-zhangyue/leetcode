4471f243159edbc667c0bc391a73727c4b0e59de
zhangyue <575865770@qq.com>
2021-08-18 10:49:35 +0800

    refactor: 有效的括号, 栈方法解析 by PHP

--- editor/cn/[20]有效的括号.php
+++ editor/cn/[20]有效的括号.php
@@ -45,6 +45,13 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution
 {
+    public $str_map = [
+        ')' => '(',
+        '}' => '{',
+        ']' => '['
+    ];
+
+    public $stack;
 
     /**
      * @param String $s
@@ -54,13 +61,47 @@ class Solution
 
     function isValid($s)
     {
-        $s = str_replace(['()', '[]', '{}'], '', $s, $count); //替换合格的,再去判断剩余是否存在
+        //替换合格字符串
+//        $s = str_replace(['()', '[]', '{}'], '', $s, $count); //替换合格的,再去判断剩余是否存在
+//
+//        if ($count == 0) {
+//            return strlen($s) == 0;
+//        } else {
+//            return $this->isValid($s);
+//        }
 
-        if ($count == 0) {
-            return strlen($s) == 0;
-        } else {
-            return $this->isValid($s);
+        //方法2 使用栈 PHP SplStack栈
+        if (empty($s)) {
+            return true;
         }
+
+        //不是偶数,返回false
+        if (strlen($s) % 2 != 0) {
+            return false;
+        }
+
+        //定义一个栈
+        $this->stack = new SplStack();
+
+        //分割字符串为数组
+        $arr = str_split($s);
+
+        foreach ($arr as $value) {
+            //当为左括号时,入栈 先出现的左括号,先进栈,后匹配
+            if (!array_key_exists($value, $this->str_map)) {
+                $this->stack->push($value);
+            } else {
+                //当为右括号时,如果栈不为空且与栈顶元素相匹配,栈顶元素出栈,否则返回false
+                if (!$this->stack->isEmpty() && $this->str_map[$value] == $this->stack->top()) {
+                    $this->stack->pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        //判断栈是否为空,为空返回true,否则返回false
+        return $this->stack->isEmpty();
     }
 }
 
