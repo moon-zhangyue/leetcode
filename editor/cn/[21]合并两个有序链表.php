320da48cc0191dde2feefb34ff4fcc48c3bbe0db
zhangyue <575865770@qq.com>
2021-08-18 14:20:18 +0800

    refactor: 合并两个有序链表 迭代方法解析 by PHP

--- editor/cn/[21]合并两个有序链表.php
+++ editor/cn/[21]合并两个有序链表.php
@@ -13,6 +13,7 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
 /**
  * Definition for a singly-linked list.
  * class ListNode {
@@ -24,15 +25,64 @@
  *     }
  * }
  */
-class Solution {
+//class ListNode
+//{
+//    public $val = 0;
+//    public $next = null;
+//
+//    function __construct($val = 0, $next = null)
+//    {
+//        $this->val  = $val;
+//        $this->next = $next;
+//    }
+//}
+
+class Solution
+{
 
     /**
      * @param ListNode $l1
      * @param ListNode $l2
+     *
      * @return ListNode
      */
-    function mergeTwoLists($l1, $l2) {
-        
+    function mergeTwoLists($l1, $l2)
+    {
+        //不可以这么写
+//        $s = array_merge($l1, $l2);
+//
+//        asort($s);
+//
+//        return array_values($s);
+
+        $Head = new ListNode(null);
+
+        $cur = $Head;
+
+        while ($l1 !== null && $l2 !== null) {
+            if ($l1->val <= $l2->val) {
+                $cur->next = $l1;
+                $l1        = $l1->next;
+            } else {
+                $cur->next = $l2;
+                $l2        = $l2->next;
+            }
+            $cur = $cur->next;
+        }
+
+        if ($l1 !== null) {
+            $cur->next = $l1;
+        } elseif ($l2 !== null) {
+            $cur->next = $l2;
+        }
+
+        return $Head->next;
     }
 }
+
+$obj = new Solution();
+
+$res = $obj->mergeTwoLists([1, 2, 4], [1, 3, 4]);
+
+var_dump($res);
 //leetcode submit region end(Prohibit modification and deletion)
