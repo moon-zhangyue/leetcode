0275ec5cc6b18592a0a355e458d6613f1f88e474
zhangyue <575865770@qq.com>
2021-08-18 14:52:13 +0800

    refactor: 合并两个有序链表 递归方法解析 by PHP

--- editor/cn/1.php
+++ editor/cn/1.php
@@ -23,28 +23,43 @@ class Solution
      */
     function mergeTwoLists($l1, $l2)
     {
-        $Head = new ListNode(null);
+//        $Head = new ListNode(null);
+//
+//        $cur = $Head;
+//
+//        while ($l1 !== null && $l2 !== null) {
+//            if ($l1->val <= $l2->val) {
+//                $cur->next = $l1;
+//                $l1        = $l1->next;
+//            } else {
+//                $cur->next = $l2;
+//                $l2        = $l2->next;
+//            }
+//            $cur = $cur->next;
+//        }
+//
+//        if ($l1 !== null) {
+//            $cur->next = $l1;
+//        } elseif ($l2 !== null) {
+//            $cur->next = $l2;
+//        }
+//
+//        return $Head->next;
 
-        $cur = $Head;
-
-        while ($l1 !== null && $l2 !== null) {
-            if ($l1->val <= $l2->val) {
-                $cur->next = $l1;
-                $l1        = $l1->next;
-            } else {
-                $cur->next = $l2;
-                $l2        = $l2->next;
-            }
-            $cur = $cur->next;
+        if ($l1 === null) {
+            return $l2;
+        }
+        if ($l2 === null) {
+            return $l1;
         }
 
-        if ($l1 !== null) {
-            $cur->next = $l1;
-        } elseif ($l2 !== null) {
-            $cur->next = $l2;
+        if ($l1->val < $l2->val) {
+            $l1->next = $this->mergeTwoLists($l1->next, $l2);
+            return $l1;
+        } else {
+            $l2->next = $this->mergeTwoLists($l1, $l2->next);
+            return $l2;
         }
-
-        return $Head->next;
     }
 }
 
