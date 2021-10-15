02446052726677a2e863b472246b0347fcdfd027
zhangyue <575865770@qq.com>
2021-10-15 11:18:31 +0800

    refactor: [38]外观数列 by PHP

---
+++ editor/cn/[38]countAndSay.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/10/15 10:28
+ * Module: [38]countAndSay.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer $n
+     *
+     * @return String
+     */
+    function countAndSay($n)
+    {
+        $this->countAndSay();
+    }
+}
