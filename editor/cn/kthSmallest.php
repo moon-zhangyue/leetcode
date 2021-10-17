032e708c77af1825e540667cfa2f41f10c8d2dae
zhangyue <575865770@qq.com>
2021-10-17 17:20:20 +0800

    [28]230  by PHP

---
+++ editor/cn/kthSmallest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/10/17 17:19
+ * Module: kthSmallest.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param TreeNode $root
+     * @param Integer  $k
+     *
+     * @return Integer
+     */
+    function kthSmallest($root, $k)
+    {
+
+    }
+}
