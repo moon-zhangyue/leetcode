71304db4e9ffe9aa6dacb6c50a472dea8a2fd279
zhangyue <575865770@qq.com>
2021-10-17 17:40:54 +0800

    [28]35 searchInsert  by PHP

---
+++ editor/cn/searchInsert.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/10/17 17:37
+ * Module: searchInsert.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer   $target
+     *
+     * @return Integer
+     */
+    function searchInsert(array $nums, int $target): int
+    {
+        return 0;
+    }
+}
