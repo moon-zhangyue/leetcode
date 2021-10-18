1ab388dcaea9109f0298bbbe5f850e0c4a69845c
zhangyue <575865770@qq.com>
2021-10-18 10:17:13 +0800

    refactor: [38]外观数列 官解分析 by PHP

--- editor/cn/[38]countAndSay.php
+++ editor/cn/[38]countAndSay.php
@@ -19,15 +19,17 @@ class Solution
         $str = '1';
         for ($i = 2; $i <= $n; $i++) {
             $sb    = [];
-            $start = 0;
-            $pos   = 0;
+            $start = 0;//起始位置
+            $pos   = 0;//
 
             while ($pos < strlen($str)) {
+                //开始统计每一个数字重复次数的起始-重点位置
                 while ($pos < strlen($str) && $str[$pos] === $str[$start]) {
                     $pos++;
                 }
+                //($pos - $start)--该数字重复的次数,$str{$start}--该数字
                 array_push($sb, ($pos - $start) . $str{$start});
-                $start = $pos;
+                $start = $pos;//数字变化位置,重新开始统计下一个数字
             }
             $str = (string)implode('', $sb);
         }
