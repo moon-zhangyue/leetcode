9e5c80ad4a10224250e208f70480544a23055c28
zhangyue <575865770@qq.com>
2021-10-19 09:09:13 +0800

    refactor: [211]WordDictionary 添加与搜索单词 by PHP

---
+++ editor/cn/WordDictionary.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/10/19 9:08
+ * Module: WordDictionary.php
+ */
+
+class WordDictionary
+{
+    /**
+     */
+    function __construct()
+    {
+
+    }
+
+    /**
+     * @param String $word
+     *
+     * @return NULL
+     */
+    function addWord($word)
+    {
+
+    }
+
+    /**
+     * @param String $word
+     *
+     * @return Boolean
+     */
+    function search($word)
+    {
+
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * $obj = WordDictionary();
+ * $obj->addWord($word);
+ * $ret_2 = $obj->search($word);
+ */
