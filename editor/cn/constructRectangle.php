b2e9e2516c5e0c61ffb173f8262d07d963a7268c
zhangyue <575865770@qq.com>
2021-10-23 15:47:51 +0800

    [492]构造矩形 by PHP

---
+++ editor/cn/constructRectangle.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/10/23 15:46
+ * Module: constructRectangle.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer $area
+     *
+     * @return Integer[]
+     */
+    function constructRectangle($area)
+    {
+
+    }
+}
