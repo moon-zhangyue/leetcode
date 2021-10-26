388e3296e32533211a7a3bf17c7e00c974e19fba
zhangyue <575865770@qq.com>
2021-10-26 08:44:45 +0800

    feat: [496]nextGreaterElement 下一个更大元素 by PHP

---
+++ editor/cn/[496]nextGreaterElement.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/10/26 8:43
+ * Module: [496]nextGreaterElement.php
+ */
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     *
+     * @return Integer[]
+     */
+    function nextGreaterElement($nums1, $nums2)
+    {
+
+    }
+}
