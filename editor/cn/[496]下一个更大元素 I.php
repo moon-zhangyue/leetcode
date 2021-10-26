5c8d2ebc5422a3ae80d655ed143b09c2b3f8cebd
zhangyue <575865770@qq.com>
2021-10-26 10:31:19 +0800

    feat: [496]nextGreaterElement 下一个更大元素  单调栈 by PHP

--- editor/cn/[496]下一个更大元素 I.php
+++ editor/cn/[496]下一个更大元素 I.php
@@ -73,7 +73,24 @@ class Solution
         return $arr;
 
         //二 要求下一个更大的元素，就是用单调栈解
+        $stack    = new SplStack();
+        $nums2Len = sizeof($nums2);
 
+        $arr = [];//保存nums2数组的next greater数组
+        for ($i = $nums2Len - 1; $i >= 0; $i--) {//从后往前迭代
+            while (!$stack->isEmpty() && $stack->top() <= $nums2[$i]) {
+                $stack->pop();//出栈
+            }
+            $arr[$nums2[$i]] = $stack->isEmpty() ? -1 : $stack->top();//技巧，直接用值当键，因为题目说了没有重复元素，这样后面可以用O（1）的时间复杂度找到每个nums1数组元素对应的数据
+            $stack->push($nums2[$i]);//入栈
+        }
+
+        $ret = [];//保存返回结果
+        foreach ($nums1 as $v) {
+            $ret[] = $arr[$v];
+        }
+
+        return $ret;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
