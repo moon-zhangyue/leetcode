fb398476bdcfba92a9d65b6087c4b717ce9b629c
zhangyue <575865770@qq.com>
2021-10-29 15:15:53 +0800

    [335]路径交叉

---
+++ editor/cn/[335]isSelfCrossing.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/10/29 15:14
+ * Module: [335]isSelfCrossing.php
+ */
+class Solution
+{
+
+    /**
+     * @param Integer[] $distance
+     *
+     * @return Boolean
+     */
+    function isSelfCrossing($distance)
+    {
+        return true;
+    }
+}
