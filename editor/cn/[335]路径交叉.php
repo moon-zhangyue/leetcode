8291515120863769ef79a0790f50d1d80eb777d9
zhangyue <575865770@qq.com>
2021-10-29 20:47:30 +0800

    [335]路径交叉 by PHP 滑动窗口

--- editor/cn/[335]路径交叉.php
+++ editor/cn/[335]路径交叉.php
@@ -50,7 +50,33 @@ class Solution
      */
     function isSelfCrossing($distance)
     {
+        $len = count($distance);
 
+        if ($len < 4) {
+            return false;
+        }
+
+        for ($i = 3; $i < $len; $i++) {
+            if ($distance[$i] >= $distance[$i - 2] && $distance[$i - 1] <= $distance[$i - 3]) {
+                return true;
+            }
+
+            if ($distance[$i - 1] <= $distance[$i - 3] && $distance[$i - 2] <= $distance[$i]) {
+                return true;
+            }
+            if ($i > 3 && $distance[$i - 1] == $distance[$i - 3] && $distance[$i] + $distance[$i - 4] ==
+                $distance[$i - 2]) {
+                return true;
+            }
+            if ($i > 4 && $distance[$i] + $distance[$i - 4] >= $distance[$i - 2] && $distance[$i - 1] >= $distance[$i
+                - 3] - $distance[$i - 5] && $distance[$i - 1] <= $distance[$i - 3] && $distance[$i - 2] >=
+                $distance[$i - 4] &&
+                $distance[$i - 3] >= $distance[$i - 5]) {
+                return true;
+            }
+
+        }
+        return false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
