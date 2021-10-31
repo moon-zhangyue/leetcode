f1039822ca00420cae65a9880c3086dcb5c13197
zhangyue <575865770@qq.com>
2021-10-31 11:37:32 +0800

    [500]键盘行 by PHP

---
+++ editor/cn/[500]findWords.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/10/31 10:47
+ * Module: [500]findWords.php
+ */
+class Solution
+{
+
+    /**
+     * @param String[] $words
+     *
+     * @return String[]
+     */
+    function findWords(array $words)
+    {
+        $str1 = 'qwertyuiop';
+        $str2 = 'asdfghjkl';
+        $str3 = 'zxcvbnm';
+
+        foreach ($words as $k => $v) {
+            $len = strlen($v);
+
+            $
+        }
+    }
+}
+
+$words = ["Hello", "Alaska", "Dad", "Peace"];
+$class = new Solution();
+var_dump($class->findWords($words));
