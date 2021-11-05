0b8481e89986ed67f2f5886d21d99a445b9b2f6c
zhangyue <575865770@qq.com>
2021-11-05 10:06:22 +0800

    [1218]最长定差子序列.php

---
+++ editor/[1218]longestSubsequence.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/5 10:04
+ * Module: [1218]longestSubsequence.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $arr
+     * @param Integer   $difference
+     *
+     * @return Integer
+     */
+    function longestSubsequence($arr, $difference)
+    {
+
+    }
+}
