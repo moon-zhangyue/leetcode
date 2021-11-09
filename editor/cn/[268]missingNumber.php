56bbe879e62db5f4e6bf9b9afc79e090b9fbfe6c
zhangyue <575865770@qq.com>
2021-11-09 13:06:31 +0800

    [268]丢失的数字.php

---
+++ editor/cn/[268]missingNumber.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/9 11:25
+ * Module: [258]missingNumber.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     *
+     * @return Integer
+     */
+    function missingNumber($nums)
+    {
+        $arr = array_diff(range(0, count($nums)), $nums);
+
+        return array_values($arr)[0];
+    }
+}
+
+$nums  = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+$class = new Solution();
+var_dump($class->missingNumber($nums));
