b5973da34c703bbe31ce6553ebcc3d68f0e8fc9f
zhangyue <575865770@qq.com>
2021-11-10 17:30:43 +0800

    [495]提莫攻击.php

---
+++ editor/cn/[495]findPoisonedDuration.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/10 17:29
+ * Module: [495]findPoisonedDuration.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $timeSeries
+     * @param Integer   $duration
+     *
+     * @return Integer
+     */
+    function findPoisonedDuration($timeSeries, $duration)
+    {
+
+    }
+}
