f0d246b5730547be43d9b0d5dc8d75308aba6747
zhangyue <575865770@qq.com>
2021-11-11 09:46:37 +0800

    [278]第一个错误的版本.php

--- editor/cn/[278]firstBadVersion.php
+++ editor/cn/[278]firstBadVersion.php
@@ -19,15 +19,27 @@ class Solution extends VersionControl
         $end   = $n;
 
         while ($start < $end) {
-            $mid = $start + ($end - $start) / 2;
+            $mid = $start + floor(($end - $start) / 2);
 
             if ($this->isBadVersion($mid)) {// 答案在区间 [left, mid] 中
                 $end = $mid;
             } else {
-                $start = $mid++; // 答案在区间 [mid+1, right] 中
+                $start = $mid + 1; // 答案在区间 [mid+1, right] 中
             }
         }
 
         return $start;  // 此时有 left == right，区间缩为一个点，即为答案
+
+        $left  = 1;
+        $right = $n;
+        while ($left < $right) {
+            $mid = floor($left + ($right - $left) / 2);
+            if ($this->isBadVersion($mid))
+                $right = $mid;
+            else
+                $left = $mid + 1;
+        }
+        return $left;
+
     }
 }
