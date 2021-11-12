cf5e04bce52bca6b53d684fd9e90745111891310
zhangyue <575865770@qq.com>
2021-11-12 17:12:02 +0800

    [19]删除链表的倒数第N个节点.php

---
+++ editor/cn/[19]removeNthFromEnd.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/12 17:10
+ * Module: [19]removeNthFromEnd.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param ListNode $head
+     * @param Integer  $n
+     *
+     * @return ListNode
+     */
+    function removeNthFromEnd($head, $n)
+    {
+
+    }
+}
