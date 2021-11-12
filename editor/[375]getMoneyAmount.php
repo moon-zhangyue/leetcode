cfa0907714459f3e9bb6e132a335501a5500045b
zhangyue <575865770@qq.com>
2021-11-12 08:43:50 +0800

    [375]猜数字大小.php

---
+++ editor/[375]getMoneyAmount.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/12 8:40
+ * Module: [375]getMoneyAmount.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer $n
+     *
+     * @return Integer
+     */
+    function getMoneyAmount($n)
+    {
+
+    }
+}
