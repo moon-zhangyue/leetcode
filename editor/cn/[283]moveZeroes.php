df3709da99e10f3a2800b05b804f02d71cbd09c1
zhangyue <575865770@qq.com>
2021-11-12 09:01:00 +0800

    [283]移动零.php

--- editor/cn/[283]moveZeroes.php
+++ editor/cn/[283]moveZeroes.php
@@ -14,8 +14,19 @@ class Solution
      *
      * @return NULL
      */
-    function moveZeroes(&$nums)
+    function moveZeroes(array &$nums): array
     {
+        foreach ($nums as $k => $v) {
+            if ($v === 0) {
+                array_push($nums, 0);
+                unset($nums[$k]);
+            }
+        }
 
+        return $nums;
     }
 }
+
+$nums  = [0, 1, 0, 3, 12];
+$class = new Solution();
+var_dump($class->moveZeroes($nums));
