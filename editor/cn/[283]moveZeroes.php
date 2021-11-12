8ce4173d915aba150bae5f02b50a48ed22f19af7
zhangyue <575865770@qq.com>
2021-11-12 08:45:42 +0800

    [375]猜数字大小.php 未完成

---
+++ editor/cn/[283]moveZeroes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/12 8:45
+ * Module: [283]moveZeroes.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     *
+     * @return NULL
+     */
+    function moveZeroes(&$nums)
+    {
+
+    }
+}
