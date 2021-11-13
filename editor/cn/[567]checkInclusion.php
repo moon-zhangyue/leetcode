e919167ca768c7a89c485f527c5618401b0e33aa
moon-zhangyue <575865770@qq.com>
2021-11-13 12:09:37 +0800

    [567]字符串的排列.php by PHP

---
+++ editor/cn/[567]checkInclusion.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/13 上午11:58
+ * Module: [567]checkInclusion.php
+ * Please No Garbage Code!
+ */
+class Solution
+{
+
+    /**
+     * @param String $s1
+     * @param String $s2
+     * @return Boolean
+     */
+    function checkInclusion($s1, $s2)
+    {
+
+    }
+}
