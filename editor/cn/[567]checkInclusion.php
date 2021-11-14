4b1c22247f67e6bd2ea96ca93b9e064b9391efa0
zhangyue <575865770@qq.com>
2021-11-14 16:37:49 +0800

    [733]图像渲染.php by PHP

--- editor/cn/[567]checkInclusion.php
+++ editor/cn/[567]checkInclusion.php
@@ -18,10 +18,10 @@ class Solution
      */
     function checkInclusion($s1, $s2)
     {
-        $needs  = str_split($s1);
-        $needs  = array_count_values($needs);//初始化need窗口
+        $needs  = str_split($s1);//字符串切片成数组
+        $needs  = array_count_values($needs);//初始化need窗口  统计值的次数
         $window = [];
-        foreach (array_keys($needs) as $k => $v) {//初始化window窗口
+        foreach (array_keys($needs) as $v) {//初始化window窗口--返回键
             $window[$v] = 0;
         }
 
@@ -32,21 +32,23 @@ class Solution
         while ($right < $s2Len) {
             $c = $s2[$right];
             $right++;//移动右指针
-            if (isset($needs[$c])) {
+
+            if (isset($needs[$c])) {//当前字母是否在子串中存在
                 $window[$c]++;
                 if ($needs[$c] == $window[$c]) {
-                    $valid++;
+                    $valid++; //目标字母连续出现个数
                 }
             }
-            if (($right - $left) >= strlen($s1)) {//需要滑动窗口
+            if (($right - $left) >= strlen($s1)) {//需要滑动窗口,重新开始
                 if ($valid == sizeof($needs)) {//找到答案
                     return true;
                 }
                 $d = $s2[$left];
-                $left++;//移动左指针
+                $left++;//向右移动左指针
+
                 if (isset($needs[$d])) {
-                    if ($needs[$d] == $window[$d]) {
-                        $valid--;
+                    if ($needs[$d] == $window[$d]) { //该值存在子串中,但是左右指针超出子串长度
+                        $valid--; //目标字母连续出现个数 -1
                     }
                     $window[$d]--;
                 }
