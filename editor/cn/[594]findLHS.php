55bbe3d51543d75660f1f78f511663f6db52c42c
zhangyue <575865770@qq.com>
2021-11-20 11:23:42 +0800

    [594]最长和谐子序列.php by PHP

---
+++ editor/cn/[594]findLHS.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/20 10:33
+ * Module: [594]findLHS.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     *
+     * @return Integer
+     */
+    function findLHS($nums)
+    {
+
+    }
+}
