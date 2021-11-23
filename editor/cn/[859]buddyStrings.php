61f2db0aa09902311ff139ad5fe1808a48e70f62
zhangyue <575865770@qq.com>
2021-11-23 09:30:53 +0800

    [859]亲密字符串.php

--- editor/cn/[859]buddyStrings.php
+++ editor/cn/[859]buddyStrings.php
@@ -17,23 +17,38 @@ class Solution
      */
     function buddyStrings(string $s, string $goal): bool
     {
-        for ($i = 0; $i < strlen($s); $i++) {
-            $j = 0;
-            while ($j < strlen($s)) {
-                $str = $s;
-
-                $exp     = $str{$i};
-                $str{$i} = $str{$j};
-                $str{$j} = $exp;
-
-                if ($str === $goal && $j !== $i) {
-                    return true;
-                }
-
-                $j++;
-            }
+        if (strlen($s) != strlen($goal)) {
+            return false;
         }
 
+        //存字符出现次数
+        $arr = [];
+        //A与B不同的字符
+        $s_diff    = [];
+        $goal_diff = [];
+        for ($i = 0; $i < strlen($s); $i++) {
+            if ($s[$i] != $goal[$i]) {
+                array_push($s_diff, $s[$i]);
+                array_push($goal_diff, $goal[$i]);
+            }
+            $arr[$s[$i]] = isset($arr[$s[$i]]) ? $arr[$s[$i]] + 1 : 1;
+        }
+        //A与B相同
+        if (empty($s_diff)) {
+            //有某个字符 >= 2,只用直接调换相同的2个数就可
+            if (max($arr) >= 2) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif (count($s_diff) == 2) {
+            //AB不同且不同的个数 == 2，则排序比较是否相同
+            sort($goal_diff);
+            sort($s_diff);
+            if ($s_diff == $goal_diff) {
+                return true;
+            }
+        }
         return false;
     }
 }
--- editor/cn/[859]亲密字符串.php
+++ editor/cn/[859]亲密字符串.php
@@ -61,6 +61,39 @@ class Solution
      */
     function buddyStrings($s, $goal)
     {
+        if (strlen($s) != strlen($goal)) {
+            return false;
+        }
+
+        //存字符出现次数
+        $arr = [];
+        //A与B不同的字符
+        $s_diff    = [];
+        $goal_diff = [];
+        for ($i = 0; $i < strlen($s); $i++) {
+            if ($s[$i] != $goal[$i]) {
+                array_push($s_diff, $s[$i]);
+                array_push($goal_diff, $goal[$i]);
+            }
+            $arr[$s[$i]] = isset($arr[$s[$i]]) ? $arr[$s[$i]] + 1 : 1;
+        }
+        //A与B相同
+        if (empty($s_diff)) {
+            //有某个字符 >= 2,只用直接调换相同的2个数就可
+            if (max($arr) >= 2) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif (count($s_diff) == 2) {
+            //AB不同且不同的个数 == 2，则排序比较是否相同
+            sort($goal_diff);
+            sort($s_diff);
+            if ($s_diff == $goal_diff) {
+                return true;
+            }
+        }
+        return false;
 
         //双指针 --- 超时
         for ($i = 0; $i < strlen($s); $i++) {
