28ab444a0d385f2470dd186b8278e4302e6a24fa
zhangyue <575865770@qq.com>
2021-11-28 11:00:44 +0800

    [191]位1的个数.php

---
+++ editor/cn/[191]hammingWeight.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/11/28 11:00
+ * Module: [191]hammingWeight.php
+ */
+
+class Solution
+{
+    /**
+     * @param Integer $n
+     *
+     * @return Integer
+     */
+    function hammingWeight($n)
+    {
+
+    }
+}
