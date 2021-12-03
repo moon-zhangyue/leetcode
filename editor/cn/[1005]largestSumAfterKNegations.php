ed3efeecb6ac9ee38f867955c7223973153daf3e
zhangyue <575865770@qq.com>
2021-12-03 09:17:50 +0800

    [1005]K次取反后最大化的数组和.php

---
+++ editor/cn/[1005]largestSumAfterKNegations.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/12/3 8:50
+ * Module: [1005]largestSumAfterKNegations.php
+ */
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer   $k
+     *
+     * @return Integer
+     */
+    function largestSumAfterKNegations($nums, $k)
+    {
+
+    }
+}
