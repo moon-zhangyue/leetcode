f6141fc17f4d95fc9247e857eca1d9dab875d9e9
zhangyue <575865770@qq.com>
2021-12-09 10:49:51 +0800

    [794]有效的井字游戏.php

---
+++ editor/cn/[794]validTicTacToe.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/12/9 10:49
+ * Module: [794]validTicTacToe.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param String[] $board
+     *
+     * @return Boolean
+     */
+    function validTicTacToe($board)
+    {
+
+    }
+}
