e5541c99d28209e2b5a34ed75c9f6f067552c9c1
zhangyue <575865770@qq.com>
2021-12-13 10:26:31 +0800

    [5]最长回文子串.php

--- editor/cn/[5]最长回文子串.php
+++ editor/cn/[5]最长回文子串.php
@@ -50,8 +50,41 @@ class Solution {
      * @param String $s
      * @return String
      */
-    function longestPalindrome($s) {
+    function longestPalindrome($s)
+    {
+        //中心扩散法
+        $len = strlen($s);
+        if ($len < 2) return $s;
 
+        $max_len = 0;
+
+        //从数组第一个记录起始位置，第二位记录长度
+        $res = [];
+
+        for ($i = 0; $i < $len - 1; $i++) {
+            $odd  = $this->centerSpread($s, $i, $i);
+            $even = $this->centerSpread($s, $i, $i + 1);
+            $max  = $odd[1] > $even[1] ? $odd : $even;
+            if ($max[1] > $max_len) {
+                $res     = $max;
+                $max_len = $max[1];
+            }
+        }
+        return substr($s, $res[0], $max_len);
+    }
+
+    function centerSpread(string $s, int $left, int $right)
+    {
+        $len = strlen($s);
+        while ($left >= 0 && $right < $len) {
+            if ($s{$left} == $s{$right}) {
+                $left--;  //left向前移动
+                $right++; //right向后移动
+            } else {
+                break;
+            }
+        }
+        return [$left + 1, $right - $left - 1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
