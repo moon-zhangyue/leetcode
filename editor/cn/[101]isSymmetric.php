7977710399c921b8a172eb05805595182683530b
zhangyue <575865770@qq.com>
2021-12-15 10:09:19 +0800

    feat: [101]对称二叉树

--- editor/cn/[101]isSymmetric.php
+++ editor/cn/[101]isSymmetric.php
@@ -16,16 +16,41 @@ class Solution
      */
     public function isSymmetric($root)
     {
-        return $this->check($root, $root);
-    }
+        //迭代
+        if ($root === null) return true;
+        if ($root->left === null && $root->right === null) return true;
+        if ($root->left === null || $root->right === null) return false;
 
-    private function check($left, $right)
-    {
-        if ($left === null && $right === null) return true;
-        if ($left === null || $right === null) return false;
-        if ($left->val !== $right->val) return false;
+        $queue = new SplQueue();
+        $queue->enqueue($root->left);
+        $queue->enqueue($root->right);
 
-        return $this->check($left->left, $right->right)
-            && $this->check($left->right, $right->left);
+        // 首先从队列中拿出两个节点 (left 和 right) 比较
+        // 将 left 的 left 节点和 right 的 right 节点放入队列
+        // 将 left 的 right 节点和 right 的 left 节点放入队列
+        // 时间复杂度是 O(n), 复杂度是 O(n)
+        while ($queue->count() > 1) {
+            $left  = $queue->dequeue();
+            $right = $queue->dequeue();
+            if ($left->val !== $right->val) return false;
+
+            if ($left->left !== null) {
+                if ($right->right === null) return false;
+                $queue->enqueue($left->left);
+                $queue->enqueue($right->right);
+            } elseif ($right->right !== null) {
+                return false;
+            }
+
+            if ($left->right !== null) {
+                if ($right->left === null) return false;
+                $queue->enqueue($left->right);
+                $queue->enqueue($right->left);
+            } elseif ($right->left !== null) {
+                return false;
+            }
+        }
+
+        return $queue->isEmpty();
     }
 }
