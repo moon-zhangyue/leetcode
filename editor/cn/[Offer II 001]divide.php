8460a334f5edecca0355a92336c521b42ade28de
zhangyue <575865770@qq.com>
2021-12-17 08:59:00 +0800

    feat: 剑指 Offer II 001. 整数除法

---
+++ editor/cn/[Offer II 001]divide.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/12/17 8:56
+ * Module: [Offer 001]divide.php
+ */
+class Solution
+{
+
+    /**
+     * @param Integer $a
+     * @param Integer $b
+     *
+     * @return Integer
+     */
+    function divide($a, $b)
+    {
+
+    }
+}
---
+++ editor/cn/[剑指 Offer II 001] 整数除法.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/12/17 8:55
+ * Module: [剑指 Offer II 001] 整数除法.php
+ */
