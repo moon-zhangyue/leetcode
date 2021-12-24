3fe31f1382bb5e650e99074dde66e37c094d77e0
zhangyue <575865770@qq.com>
2021-12-24 08:57:01 +0800

    feat: [137]只出现一次的数字 II.php

---
+++ editor/cn/[137]singleNumber.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/12/24 8:53
+ * Module: [137]singleNumber.php
+ */
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     *
+     * @return Integer
+     */
+    function singleNumber($nums)
+    {
+
+    }
+}
+
+$nums  = [0, 1, 0, 1, 0, 1, 99];
+$class = new Solution();
+var_dump($class->singleNumber($nums));
