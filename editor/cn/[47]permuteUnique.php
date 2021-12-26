8df0406fe11da0a28d435a9bf3f4ba72681182eb
zhangyue <575865770@qq.com>
2021-12-26 16:11:47 +0800

    [47]全排列 II.php

---
+++ editor/cn/[47]permuteUnique.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2021/12/26 16:05
+ * Module: [47]permuteUnique.php
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     *
+     * @return Integer[][]
+     */
+    function permuteUnique($nums)
+    {
+
+    }
+}
