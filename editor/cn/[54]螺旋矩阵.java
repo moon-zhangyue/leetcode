95183946d369ea977d51c7c6fe28afdad4d40c61
moon <575865770@qq.com>
2022-01-24 15:45:00 +0800

    test

---
+++ editor/cn/[54]螺旋矩阵.java
@@ -0,0 +1,39 @@
+<?php
+//给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+//输出：[1,2,3,6,9,8,7,4,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+//输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 10 
+// -100 <= matrix[i][j] <= 100 
+// 
+// Related Topics 数组 矩阵 模拟 👍 967 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
