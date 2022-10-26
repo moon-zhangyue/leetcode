88e959627a727789cb6e0728cb64db403d18b23f
zhangyue <575865770@qq.com>
2022-10-26 09:10:28 +0800

    perf: [728]自除数

---
+++ editor/cn/[728]selfDividingNumbers.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2022/10/26 9:09
+ * Module: [728]selfDividingNumbers.php
+ */
+class Solution
+{
+
+    /**
+     * @param Integer $left
+     * @param Integer $right
+     *
+     * @return Integer[]
+     */
+    function selfDividingNumbers($left, $right)
+    {
+
+    }
+}
---
+++ editor/cn/[728]自除数.php
@@ -0,0 +1,50 @@
+<?php
+//自除数 是指可以被它包含的每一位数整除的数。 
+//
+// 
+// 例如，128 是一个 自除数 ，因为 128 % 1 == 0，128 % 2 == 0，128 % 8 == 0。 
+// 
+//
+// 自除数 不允许包含 0 。 
+//
+// 给定两个整数 left 和 right ，返回一个列表，列表的元素是范围 [left, right] 内所有的 自除数 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：left = 1, right = 22
+//输出：[1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：left = 47, right = 85
+//输出：[48,55,66,77]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= left <= right <= 10⁴ 
+// 
+// Related Topics 数学 👍 248 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * @param Integer $left
+     * @param Integer $right
+     * @return Integer[]
+     */
+    function selfDividingNumbers($left, $right) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
