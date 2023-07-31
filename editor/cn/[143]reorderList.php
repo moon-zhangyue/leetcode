db99dd64c1b2c5843a0843834766c2304b356474
moon-zhangyue <575865770@qq.com>
2023-07-31 14:50:53 +0800

    太困了.......

---
+++ editor/cn/[143]reorderList.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: SpiRit-Moon
+ * Time: 2023/7/31 11:16
+ * Module: [143]reorderList.php
+ */
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution
+{
+    /**
+     * @param ListNode $head
+     *
+     * @return NULL
+     */
+    function reorderList($head)
+    {
+
+    }
+}
---
+++ editor/leetcode/editor/cn/[143]重排链表.php
@@ -0,0 +1,66 @@
+//给定一个单链表 L 的头节点 head ，单链表 L 表示为： 
+//
+// 
+//L0 → L1 → … → Ln - 1 → Ln
+// 
+//
+// 请将其重新排列后变为： 
+//
+// 
+//L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → … 
+//
+// 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：head = [1,2,3,4]
+//输出：[1,4,2,3] 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[1,5,2,4,3] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表的长度范围为 [1, 5 * 10⁴] 
+// 1 <= node.val <= 1000 
+// 
+//
+// Related Topics 栈 递归 链表 双指针 👍 1275 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return NULL
+     */
+    function reorderList($head) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
